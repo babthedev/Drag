828ab30c8b02976b871a56d5960d12d305120d03
Abdulrahman Bello <belloabdulrahman13@gmail.com>
2024-04-11 10:49:10 +0100

    UI fixes

--- src/homepage/Hero.jsx
+++ src/homepage/Hero.jsx
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 import manSmileLight from "../assets/mansmillingwhite.png"
 const Hero = () => {
   return (
-    <div className='flex flex-col-reverse lg:flex-row gap-2 px-6 md:px-16 lg:px-36'>
+    <div className='flex flex-col-reverse lg:flex-row gap-2 px-6 md:px-16 lg:px-36 justify-center'>
         <div className='lg:w-1/2 w-full'>
-            <h1 className='font-bold hero-text  md:mt-16 mt-4 lg:mt-24'>Let's Elevate Your Financial Journey to Greater Heights!</h1>
-            <p className='text-base lg:text-2xl mt-2 md:font-semibold'>Welcome to DRAG, your all-in-one solution for effortless financial management with empowered savings.</p>
+            <h1 className='font-bold hero-text text-3xl  md:mt-16 mt-4 lg:mt-24'>Let's Elevate Your Financial Journey to Greater Heights!</h1>
+            <p className='text-xl mt-2 md:font-semibold'>Welcome to DRAG, your all-in-one solution for effortless financial management with empowered savings.</p>
             <div className='flex text-lg md:text-2xl items-center gap-4 mt-8 md:font-semibold'>
                 <Link to="/signup">
                     <h2 className='border-2 py-2 px-4 md:py-4 md:px-8 rounded-full border-gray-200 bg-gray-200 hover:bg-black hover:border-black hover:text-white text-black'>Get Started</h2>
